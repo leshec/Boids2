64bc9073bd03ffee4b3028a8fbc902ddc0661873
richard <richmcewan@hotmail.com>
2022-04-12 18:23:31 +0100

    Initial commit

---
+++ core/src/main/java/com/badlogic/gdx/Boids2.java
@@ -0,0 +1,124 @@
+package com.badlogic.gdx;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+//import com.badlogic.gdx.utils.Array;
+
+import java.util.ArrayList;
+
+import space.earlygrey.shapedrawer.ShapeDrawer;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Boids2 extends ApplicationAdapter {
+	//note  from Lwjgl3Launcher in build: configuration.setWindowedMode(640, 480);
+	//shall I remove these duplicated: TO DO check use
+	private final float WIDTH = 640;
+	private final float HEIGHT = 480;
+	private SpriteBatch batch;
+	private Texture image;
+	private ShapeDrawer drawer;
+	private OrthographicCamera camera;
+	private Vector2 motion1;
+	private Vector2 motion2;
+	private Vector2 motion3;
+	private Vector2 motion4;
+	private Vehicle vehicle;
+	private ArrayList<Vehicle> vehicles;
+	private int numberOfBoids =30;
+
+//TO DO contribute to lib gdx vector methods are lacking
+
+	@Override
+	public void create() {
+
+		//initialise ArrayList
+		vehicles = new ArrayList<Vehicle>();
+
+		for(int i = 0; i<numberOfBoids; i++){
+			Vehicle vehicle = new Vehicle(MathUtils.random(0, WIDTH), MathUtils.random(0,HEIGHT));
+			vehicles.add(vehicle);
+		}
+
+		Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+		pixmap.setColor(Color.WHITE);
+		pixmap.drawPixel(0, 0);
+		Texture texture = new Texture(pixmap); //remember to dispose of later
+		pixmap.dispose(); //
+		TextureRegion region = new TextureRegion(texture, 0, 0, 1, 1);
+
+		// create the camera and the SpriteBatch
+		camera = new OrthographicCamera();
+		//note  from Lwjgl3Launcher in build: configuration.setWindowedMode(640, 480);
+		camera.setToOrtho(false, 640, 480);
+		batch = new SpriteBatch();
+		//image = new Texture("libgdx.png");
+		drawer = new ShapeDrawer(batch, region);
+
+		//stock vectors for testing
+		motion1 = new Vector2(1, 0);
+		motion2 = new Vector2(0,1);
+		motion3 = new Vector2(-1, 0);
+		motion4 = new Vector2(0,-1);
+
+
+
+
+	}
+
+	@Override
+	public void render() {
+		//note  from Lwjgl3Launcher in build: configuration.setWindowedMode(640, 480);
+		Gdx.gl.glClearColor(0.15f, 0.15f, 0.2f, 1f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		// tell the camera to update its matrices.
+		camera.update();
+
+		// tell the SpriteBatch to render in the
+		// coordinate system specified by the camera.
+		batch.setProjectionMatrix(camera.combined);
+
+		draw();
+		update();
+
+	}
+
+	public void draw(){
+
+		//drawer.circle(5,5,5,2);
+
+		for(Vehicle vehicle: vehicles){
+			batch.begin();
+			drawer.circle(vehicle.getX(), vehicle.getY(), vehicle.r, vehicle.lineWidth);
+			batch.end();
+		}
+	}
+
+
+	public void update(){
+
+		for(Vehicle vehicle: vehicles){
+			vehicle.separate(vehicles);
+			vehicle.align(vehicles);
+			vehicle.cohesion(vehicles);
+			vehicle.update();
+			vehicle.borders();
+		}
+	}
+
+
+	@Override
+	public void dispose() {
+		batch.dispose();
+		image.dispose();
+	}
+}
---
+++ core/src/main/java/com/badlogic/gdx/Vehicle.java
@@ -0,0 +1,166 @@
+package com.badlogic.gdx;
+
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import java.util.ArrayList;
+
+
+public class Vehicle extends Vector2 {
+    //duplicate WIDTH/HEIGHT
+    //can I pull in from the LGW context?
+    private final float WIDTH = 640;
+    private final float HEIGHT = 480;
+    private Vector2 position;
+    private Vector2 velocity;
+    private Vector2 acceleration;
+    float maxspeed;
+    float maxforce;
+    float r;
+    float lineWidth;
+    //private ArrayList<Vehicle> vehicles;
+    int debugger =0;
+
+
+    public Vehicle(float x, float y) {
+        position = new Vector2(x, y);
+        acceleration = new Vector2(MathUtils.random(-1,1),MathUtils.random(-1,1));
+        velocity = new Vector2(0, 0);
+
+        r = 5.0f;
+        lineWidth = 2.0f;
+        maxforce = 0.05f;
+        maxspeed = 3f;
+    }
+
+    void update() {
+        velocity.add(acceleration);
+        velocity.limit(maxspeed);
+        position.add(velocity);
+        acceleration.scl(0);
+    }
+
+
+    public float getX() {
+        return position.x;
+    }
+
+    public float getY() {
+        return position.y;
+    }
+
+    void applyForce(Vector2 force) {
+        acceleration.add(force);
+    }
+
+    public void borders() {
+
+        if (position.x < -r) position.x = WIDTH + r;
+        if (position.y < -r) position.y = HEIGHT + r;
+        if (position.x > WIDTH + r) position.x = -r;
+        if (position.y > HEIGHT + r) position.y = -r;
+    }
+
+    public void align(ArrayList<Vehicle> vehicles) {
+
+        float neighbordist = 20;
+        Vector2 sum = new Vector2(0,0);
+        int count = 0;
+        for (Vehicle vehicle : vehicles) {
+            float d = Vector2.dst(position.x, position.y,vehicle.position.x, vehicle.position.y);
+            if ((d > 0f) && (d < neighbordist)) {
+                sum.add(vehicle.velocity);
+                count++;
+            }
+        }
+
+        if (count > 0) {
+            sum.scl(1 /((float) count));
+            sum.nor();
+            sum.scl(maxspeed);
+            Vector2 steer = sum.sub(velocity);
+            steer.limit((maxforce));
+            this.applyForce(steer);
+        }
+        /*else {
+            this.applyForce(new Vector2());
+        }
+
+         */
+    }
+
+
+    public void separate(ArrayList<Vehicle> vehicles) {
+        //Make this a global variable, maybe?
+        float desiredSeperation = 40;
+        Vector2 sum = new Vector2();
+        int count = 0;
+        for (Vehicle vehicle : vehicles) {
+            //bug here
+            float d = Vector2.dst(position.x, position.y,vehicle.position.x, vehicle.position.y);
+            if ((d > 0f) && (d < desiredSeperation)) {
+                //must be a better way
+                Vector2 diff = new Vector2(position.x -vehicle.position.x,position.y-vehicle.position.y);
+                diff.sub(vehicle);
+                diff.nor();
+                diff.scl(1/d); //a hack to replace division method
+                sum.add(diff);
+                count +=1;
+            }
+        }
+
+        if (count > 0) {
+            sum.setLength2(maxspeed);
+            Vector2 steer = sum.sub(velocity);
+            steer.limit((maxspeed));
+            this.applyForce(steer);
+        }
+}
+
+    public void cohesion(ArrayList<Vehicle> vehicles) {
+        float neighbordist = 50;
+        Vector2 sum = new Vector2(0,0);
+        int count = 0;
+        for (Vehicle vehicle : vehicles) {
+            float d = Vector2.dst(position.x, position.y,vehicle.position.x, vehicle.position.y);
+            if ((d > 0f) && (d < neighbordist)) {
+                //must be a better way
+                sum.add(vehicle.velocity);
+                count++;
+            }
+        }
+
+        if (count > 0) {
+            sum.scl(1 /((float) count));
+            seek(sum);
+        }
+
+        /*else {
+            this.applyForce(new Vector2());
+        }
+
+         */
+    }
+
+    private void seek(Vector2 target) {
+        Vector2 desired = new Vector2(target.x-position.x,target.y-position.y);
+        desired.nor();
+        desired.scl(maxspeed);
+        Vector2 steer = desired.sub(velocity);
+        steer.limit(maxforce);
+        System.out.println("cohese force applied via seek");
+        this.applyForce(steer);
+    }
+
+
+//end of class
+}
+
+    
+
+
+
+
+
+
+
+
